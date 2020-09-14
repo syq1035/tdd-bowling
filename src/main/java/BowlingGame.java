6780e20fb95f3ebb96dd0d82edd6be5b18a2d8a1
syq <1035394794@qq.com>
2020-09-14 20:55:40 +0800

    [syq] deal with illegal input throw exception

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,20 +2,28 @@ import java.util.List;
 
 public class BowlingGame {
 
-    public int getTotalScore(List<Integer> score) {
+    public int getTotalScore(int[] score) throws Exception {
         int totalScore = 0;
-        for (int index = 0; index < score.size(); index++) {
+        int round = 0;
+        for (int index = 0; index < score.length-1; index++) {
             int oneRoundScore = 0;
-            if (score.get(index).equals(10)) {
-                oneRoundScore = 10 + score.get(index+1) + score.get(index+2);
-            } else if (score.get(index) + score.get(index+1) == 10) {
-                oneRoundScore = 10 + score.get(index+2);
+            if (score[index] == 10 && index < score.length-1) {
+                oneRoundScore = 10 + score[index+1] + score[index+2];
+            } else if ((score[index] + score[index+1] == 10) && index < score.length-2) {
+                oneRoundScore = 10 + score[index+2];
                 index++;
             } else {
-                oneRoundScore = score.get(index) + score.get(index+1);
+                oneRoundScore = score[index] + score[index+1];
                 index++;
             }
+            round++;
+            if(round == 10 && index < score.length-1){
+                throw new Exception("投球轮数超过10");
+            }
             totalScore += oneRoundScore;
+        }
+        if(round < 10) {
+            throw new Exception("投球轮数小于10");
         }
         return totalScore;
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,27 +4,45 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BowlingGameTest {
 
     @Test
-    public void should_return_total_score_when_not_knock_all_down_after_two_pitches() {
+    public void should_return_total_score_when_not_knock_all_down_after_two_pitches() throws Exception {
         BowlingGame bowlingGame = new BowlingGame();
-        List<Integer> score = Arrays.asList(3, 4, 5, 4, 2, 6, 7, 2, 5, 3, 4, 3, 6, 3, 8, 1, 0, 9, 8, 1);
+        int[] score = {3, 4, 5, 4, 2, 6, 7, 2, 5, 3, 4, 3, 6, 3, 8, 1, 0, 9, 8, 1};
         assertEquals(84, bowlingGame.getTotalScore(score));
     }
 
     @Test
-    public void should_return_total_score_when_strike() {
+    public void should_return_total_score_when_strike() throws Exception {
         BowlingGame bowlingGame = new BowlingGame();
-        List<Integer> score = Arrays.asList(10, 5, 4, 2, 6, 10, 5, 3, 4, 3, 10, 10, 0, 9, 8, 1);
+        int[] score = {10, 5, 4, 2, 6, 10, 5, 3, 4, 3, 10, 10, 0, 9, 8, 1};
         assertEquals(126, bowlingGame.getTotalScore(score));
     }
 
     @Test
-    public void should_return_total_score_when_spare() {
+    public void should_return_total_score_when_spare() throws Exception {
         BowlingGame bowlingGame = new BowlingGame();
-        List<Integer> score = Arrays.asList(10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 9, 8, 1);
+            int[] score = {10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 9, 8, 1};
         assertEquals(141, bowlingGame.getTotalScore(score));
+    }
+
+    @Test
+    public void should_throw_exception_when_input_illegal() throws Exception {
+        BowlingGame bowlingGame = new BowlingGame();
+        //未投完十轮
+        int[] score1 = {10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 9, 8};
+        //投完十轮后多投了球
+        int[] score2 = {10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 9, 8, 1, 1};
+        //第十轮全中后，未投完最后俩球
+        int[] score3 = {10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 10, 1};
+        //第十轮全中后，投球超过两个
+        int[] score4 = {10, 5, 5, 2, 6, 10, 5, 3, 4, 3, 10, 10, 1, 10, 2, 3, 4};
+        assertThrows(Exception.class, () -> bowlingGame.getTotalScore(score1));
+        assertThrows(Exception.class, () -> bowlingGame.getTotalScore(score2));
+        assertThrows(Exception.class, () -> bowlingGame.getTotalScore(score3));
+        assertThrows(Exception.class, () -> bowlingGame.getTotalScore(score4));
     }
 }
